90feb7892a24d343f1912950cee517ba5c0ceab4
Jacob Boon <jacob@wearelandscape.nl>
2018-04-11 09:06:36 +0200

    Newer Map component

--- webapp/src/components/Map.js
+++ webapp/src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as d3 from 'd3'
+import * as d3 from 'd3'
 import * as L from 'leaflet'
 import * as $ from 'jquery'
 import _ from 'lodash'
@@ -13,11 +13,6 @@ class Map extends React.Component {
   }
 
   update() {
-    const onSelect = this.props.onSelect.bind(this)
-    // vanilla JS goes here
-    const { data } = this.props
-    if (data == null) return
-
     var fieldid,
       activeFeature,
       map = {},
@@ -25,20 +20,13 @@ class Map extends React.Component {
       bbox,
       offset = 0,
       busy = false,
+      farmcontrol,
+      farm = null,
       filecontrol
-    var fieldsLayer, fieldLayer
+    var fieldsLayer, fieldLayer, farmLayer
     var baseUrl = 'https://agrodatacube.wur.nl/api/v1/rest'
     var startView = [52.52, 4.815],
       startZoom = 14
-    var enumKruidenrijkheid = {
-      '0': 'niet',
-      '1': 'niet',
-      '2': 'bijna',
-      '3': 'bijna',
-      '4': 'wel',
-      '5': 'wel'
-    }
-
     function mousePropagationKiller(div, map) {
       if (!L.Browser.touch) {
         L.DomEvent.disableClickPropagation(div)
@@ -46,7 +34,7 @@ class Map extends React.Component {
         L.DomEvent.on(div, 'mousewheel', L.DomEvent.stopPropagation)
       } else {
         L.DomEvent.on(div, 'click dblclick', function(ev) {
-          if (ev.type === 'div') {
+          if (ev.type == 'div') {
             L.DomEvent.stopPropagation(ev)
             L.DomEvent.stop(ev)
           }
@@ -61,11 +49,74 @@ class Map extends React.Component {
         })
       }
     }
+    function getProperty(propertyName, object) {
+      var parts = propertyName.split('.'),
+        length = parts.length,
+        i,
+        property = object || this
+
+      for (i = 0; i < length; i++) {
+        property = property[parts[i]]
+      }
+
+      return property
+    }
+    function createSelect(
+      parentcomponent,
+      onChange,
+      label,
+      choices,
+      field,
+      styleFn
+    ) {
+      var selectdiv = L.DomUtil.create('div', '', parentcomponent)
+      var vraagdiv = L.DomUtil.create('div', '', selectdiv)
+      vraagdiv.innerHTML = '<b> &nbsp;' + label + ':</b>'
+      var selectcomp = L.DomUtil.create('select', '', selectdiv)
+
+      var title,
+        options =
+          '<option value="" disabled selected style="display:none;">(make a choice)</option>'
+
+      for (var j = 0; j < choices.length; j++) {
+        if (field) title = getProperty(field, choices[j])
+        else title = choices[j]
+        options +=
+          '<option class="' +
+          styleFn(choices[j]) +
+          '" value="' +
+          j +
+          '">' +
+          title +
+          '</option>'
+      }
+      selectcomp.innerHTML = options
+      selectcomp['data'] = choices
+      selectcomp['function'] = onChange
+      selectcomp.onchange = function(e) {
+        console.log(this['data'][parseInt(this.value)])
+        if (selectcomp['function'])
+          selectcomp['function'](this['data'][parseInt(this.value)])
+      }
+
+      return selectdiv
+    }
+    var enumKruidenrijkheid = { E: 'niet', D: 'niet', C: 'bijna', B: 'wel' }
+    var enumColor = { E: 'red', D: 'red', C: 'orange', B: 'green' }
+    function kruidenrijkheidStyle(feature) {
+      return {
+        color:
+          enumColor[(feature.properties.classificatie || 'A').split(' ')[0]] ||
+          'pink'
+      }
+    }
+    function kruidenrijkheidDoelstellingStyle(object) {
+      return object['kruidenrijkheid']['wel'] >= 0.2 ? 'green' : 'black'
+    }
     function selectFeature(e) {
-      onSelect(_.get(e.target, 'feature'))
       var aLayer = e.target
       var options = aLayer.options || aLayer._options
-      if (options.title === 'fields') {
+      if (options.title == 'fields') {
         activeFeature = aLayer.feature
         fieldid = activeFeature.properties['fieldid']
 
@@ -78,9 +129,6 @@ class Map extends React.Component {
             '?output_epsg=4326&page_size=50&page_offset=0',
           dataType: 'json',
           crossDomain: true,
-          // beforeSend: function( xhr ) {
-          //	xhr["token"] = MIJN_TOKEN;
-          // },
           headers: {
             'Content-Type': 'text/plain'
           },
@@ -101,22 +149,23 @@ class Map extends React.Component {
             }).addTo(map['fields'])
             controlLayers['fields'].addOverlay(fieldLayer, 'Field ' + fieldid)
             var bounds = fieldLayer.getBounds()
-            // map['files'].fitBounds(bounds, {padding: [0.1,0.1]});
+            map['fields'].fitBounds(bounds, { padding: [1, 1] })
           }
         })
       }
     }
     function zoomToFeature(e) {
       var bounds = e.target.getBounds()
-      // map['fields'].fitBounds(bounds, {padding: [0.1,0.1]});
-      // map['files'].fitBounds(bounds, {padding: [0.1,0.1]});
+      map['fields'].fitBounds(bounds, { padding: [0.1, 0.1] })
     }
     function onEachFeature(aFeature, aLayer) {
       var options = aLayer.options || aLayer._options
 
       aLayer.on({
-        click: selectFeature //,
-        // dblclick: zoomToFeature
+        // selectFeature//,
+        click:
+          // dblclick:
+          zoomToFeature
       })
       var props = '<b>' + options.title + '</b><br>'
       for (const d in aFeature.properties) {
@@ -158,9 +207,6 @@ class Map extends React.Component {
         url: url,
         dataType: 'json',
         crossDomain: true,
-        // beforeSend: function( xhr ) {
-        //	xhr["token"] = MIJN_TOKEN;
-        // },
         headers: {
           'Content-Type': 'text/plain'
         },
@@ -215,13 +261,7 @@ class Map extends React.Component {
           url: url,
           dataType: 'json',
           crossDomain: true,
-          // beforeSend: function( xhr ) {
-          //	xhr.setRequestHeader("token", MIJN_TOKEN);
-          // },
-          /* xhrFields: {
-            withCredentials: true
-          },
-          */ headers: {
+          headers: {
             'Content-Type': 'text/plain'
           },
           success: function(response, textStatus) {
@@ -250,59 +290,7 @@ class Map extends React.Component {
         activeBounds = bounds
       }
     }
-    function geocodeSearch(searchStr) {
-      var center = map['fields'].getCenter()
-      var url =
-        'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates' +
-        '?SingleLine=' +
-        searchStr +
-        '&f=json' +
-        '&location=' +
-        center +
-        '&distance=2' + // '3218.69' = 2 meters
-        '&outSR=4326' + // '102100'  = web mercator
-        // '&searchExtent='+bbox+
-        // '&outFields=Loc_name'+
-        '&maxLocations=1'
-      /* PDOK locatieserver => json.response.docs[0].centroide_ll
-  https://geodata.nationaalgeoregister.nl/locatieserver/v3/free?q=langbroek&rows=1
-  {"response":{"numFound":1167,"start":0,"maxScore":26.717785,"docs":
-  [
-  {"bron":"BAG","identificatie":"1303","provinciecode":"PV26","woonplaatscode":"1303","type":"woonplaats","woonplaatsnaam":"Langbroek","provincienaam":"Utrecht","centroide_ll":"POINT(5.35005208 52.00328214)","gemeentecode":"0352","weergavenaam":"Langbroek, Wijk bij Duurstede, Utrecht","provincieafkorting":"UT","centroide_rd":"POINT(152448.962 446101.503)","id":"wpl-0562d68d3ba1b866e6c787f2cab1ca56","gemeentenaam":"Wijk bij Duurstede","score":26.717785}
-  ]}}
-  */
-      var xhttp
-      xhttp = new XMLHttpRequest()
-
-      xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-          var response = JSON.parse(this.responseText)
-          // this.responseXML;
-          var searchExt = response.candidates[0].extent
-          // var spatialRef= response.spatialReference.wkid;
-          var bounds = [
-            [searchExt.ymin, searchExt.xmin],
-            [searchExt.ymax, searchExt.xmax]
-          ]
-          map['fields']
-            .fitBounds(bounds, { padding: [0, 0] })
-            .setZoom(startZoom - 1)
-        }
-      }
-      xhttp.open('GET', url, true)
-      xhttp.send()
-    }
-    if (document.querySelector('#searchForm'))
-      document
-        .querySelector('#searchForm')
-        .addEventListener('submit', function(e) {
-          geocodeSearch(e.target.children.namedItem('search').value)
-          e.preventDefault()
-          return false // prevent page reload
-        })
-
     map['fields'] = L.map(this.anchor).setView(startView, startZoom)
-    // map['files'] = L.map('map2',{attributionControl: false, zoomControl: false}).setView(startView, startZoom+1);
 
     var baseLayers = {
       'Open Street Map': L.tileLayer(
@@ -323,15 +311,169 @@ class Map extends React.Component {
       )
     }
     map['fields'].addLayer(baseLayers['Open Street Map'])
-    map['fields'].on('moveend', onMoveEnd)
+    // map['fields'].on('moveend', onMoveEnd);
+
+    // filecontrol left
+    filecontrol = L.control({ position: 'topleft' })
+    filecontrol.handleReaderAbort = function(e) {
+      alert('File read cancelled')
+    }
+    filecontrol.handleReaderLoadend = function(ev) {
+      // check ready
+      if (ev.target.readyState == FileReader.DONE) {
+        // DONE == 2
+        console.log(ev.target.filename)
+        if (ev.target.result) {
+          var parts = ev.target.filename.split('.')
+          var extensie = parts[parts.length - 1].toLowerCase()
+          if (extensie == 'json' || extensie == 'geojson') {
+            var geojson = JSON.parse(ev.target.result)
+            var keys_data = d3
+              .nest()
+              .key(function(d) {
+                return 'farm ' + d.properties.Nummer % 10
+              })
+              .entries(geojson.features)
+              .sort(function(x, y) {
+                return d3.ascending(x.key, y.key)
+              })
+            // for each farm (identified by DEELN_ID)
+            // calculate percentages of kruidenrijkheid
+            keys_data.forEach(function(item, index) {
+              item['Opp.'] = 0
+              item['kruidenrijkheid'] = { niet: 0, bijna: 0, wel: 0 }
+
+              // sum area and NET_AREA for al fields of a farm
+              item.values.forEach(function(subitem, subindex) {
+                item['Opp.'] += subitem.properties['Opp.']
+                item['kruidenrijkheid'][
+                  enumKruidenrijkheid[
+                    (subitem.properties.classificatie || 'A').split(' ')[0]
+                  ] || 'niet'
+                ] +=
+                  subitem.properties['Opp.']
+              })
+              if (item['Opp.'] > 0) {
+                item['kruidenrijkheid']['niet'] /= item['Opp.']
+                item['kruidenrijkheid']['bijna'] /= item['Opp.']
+                item['kruidenrijkheid']['wel'] /= item['Opp.']
+              }
+            })
+            if (keys_data.length > 0) {
+              createSelect(
+                farmcontrol.farms,
+                onChangeFarm,
+                'Farms',
+                keys_data,
+                'key',
+                kruidenrijkheidDoelstellingStyle
+              )
+            }
+            /* var geojsonLayer = L.geoJson(geojson, {
+  						title: parts[parts.length - 2],
+  						style: {color: 'pink'},
+  						pointToLayer: function(geoJsonPoint, latlng) {
+  							return L.circleMarker(latlng, {title: parts[parts.length - 2], weight:1, radius: 2});
+  						},
+  						onEachFeature: onEachFeature}
+  					).addTo(map['fields']);
+  					controlLayers['fields'].addOverlay(geojsonLayer, parts[parts.length - 2]);
+  					*/
+          }
+        } else {
+          alert('empty file')
+        }
+      }
+    }
+    filecontrol.handleFileSelect = function(evt) {
+      if (evt.target.files.length > 0) {
+        console.log(
+          evt.target.files.length,
+          evt.target.files[0].name,
+          evt.target.files[evt.target.files.length - 1].name
+        )
+        // checks
+        for (var f = evt.target.files.length - 1; f > -1; f--) {
+          var file = evt.target.files[f]
+          if (file.size == 0) {
+            alert('empty file: ' + file.name)
+            return
+          }
+          var fileparts = file.name.split('.')
+          var extensie = fileparts[fileparts.length - 1].toLowerCase()
+          if (extensie != 'json' && extensie != 'geojson') {
+            alert('wrong file type: ' + extensie)
+            return
+          }
+        }
+
+        var reader = []
+        for (var f = evt.target.files.length - 1; f > -1; f--) {
+          reader.push(new FileReader())
+        }
+        for (var f = evt.target.files.length - 1; f > -1; f--) {
+          // reader.onerror = errorHandler;
+          // reader.onprogress = updateProgress; // + reader.onloadstart, reader.onload
+          reader[f].onabort = filecontrol.handleReaderAbort
+          reader[f].onloadend = filecontrol.handleReaderLoadend
+          reader[f]['filename'] = evt.target.files[f].name
+          var file = evt.target.files[f]
+          var fileparts = file.name.split('.')
+          var extensie = fileparts[fileparts.length - 1].toLowerCase()
+          if (extensie == 'json' || extensie == 'geojson')
+            reader[f].readAsText(file)
+        }
+        evt.target.value = ''
+        // bug recurrent filebox in edge
+        // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/8282613/
+        // opgelost in: Microsoft EdgeHTML 15.15xxx
+      }
+    }
+    filecontrol.onAdd = function(map) {
+      this._div = L.DomUtil.create('div', 'filecontrol')
+      mousePropagationKiller(this._div, map)
+      this._lbl = L.DomUtil.create('label', 'fileContainer', this._div)
+      this._lbl.textContent = '+ file geojson'
+      this._files = L.DomUtil.create('input', 'files', this._lbl)
+      this._files.setAttribute('type', 'file')
+      this._files.setAttribute('accept', '.json,.geojson')
+      return this._div
+    }
+    filecontrol.addTo(map['fields'])
+    filecontrol._files.addEventListener('change', filecontrol.handleFileSelect)
+
+    // farmcontrol left
+    function onChangeFarm(object) {
+      farm = object.key
+      if (farmLayer) {
+        farmLayer.clearLayers()
+        // controlLayers['fields'].removeLayer(farmLayer);
+      }
+      farmLayer = L.geoJson(
+        { type: 'FeatureCollection', features: object.values },
+        {
+          title: farm,
+          style: kruidenrijkheidStyle,
+          onEachFeature: onEachFeature
+        }
+      ).addTo(map['fields'])
+      // controlLayers['fields'].addOverlay(farmLayer, farm);
+      map['fields'].flyToBounds(farmLayer.getBounds(), { padding: [0.5, 0.5] })
+    }
+    farmcontrol = L.control({ position: 'topleft' })
+    farmcontrol.onAdd = function(map) {
+      this._div = L.DomUtil.create('div', 'farmcontrol')
+      mousePropagationKiller(this._div, map)
+      this.farms = L.DomUtil.create('div', 'infotools', this._div)
+      return this._div
+    }
+    farmcontrol.addTo(map['fields'])
 
     var controlLayers = {}
     controlLayers['fields'] = L.control.layers(baseLayers).addTo(map['fields'])
     controlLayers['fields'].setPosition('topleft')
-    // controlLayers['files'] = L.control.layers([]).addTo(map['files']);
-    // controlLayers['files'].setPosition('topleft');
 
-    onMoveEnd({ target: map['fields'] })
+    // onMoveEnd({target: map['fields']});
   }
 
   render() {
